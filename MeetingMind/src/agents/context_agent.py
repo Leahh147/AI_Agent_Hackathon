840cd10b5ea91af4b17e86239a7e015ab1132372
Leahh147 <leah12577@gmail.com>
2025-03-09 01:08:33 +0000

    update context agent with correct interval

--- MeetingMind/src/agents/context_agent.py
+++ MeetingMind/src/agents/context_agent.py
@@ -1,11 +1,13 @@
 import json
 from openai import OpenAI
 import asyncio
+import os
 
 class ContextAgent:
-    def __init__(self, profiles, api_key):
-        self.profiles = profiles
+    def __init__(self, minutes_agent):
+        api_key = os.getenv("API_KEY")
         self.api_key = api_key
+        self.minutes_agent = minutes_agent
         self.current_timestamp = 0
         self.listen_in = False
         self.client = OpenAI(api_key=self.api_key)
@@ -16,10 +18,10 @@ class ContextAgent:
             role_descriptions = json.load(file)
         return role_descriptions
 
-    async def analyze_context(self, lines):
+    async def analyze_context(self, lines): # if lines is empty, the agent should be able to give a response of no
         try:
             completion = await self.client.chat.completions.create(
-                model="gpt-4o-mini",
+                model="gpt-4",
                 messages=[
                     {"role": "system", "content": "You are a helpful assistant."},
                     {
@@ -40,7 +42,8 @@ class ContextAgent:
             print(e.response)
         return "no"
 
-    async def should_listen_in(self, lines):
+    async def should_listen_in(self):
+        lines = self.get_minutes_within_current_topic()
         context_analysis = await self.analyze_context(lines)
         if context_analysis.lower() == "yes":
             self.listen_in = True
@@ -53,3 +56,8 @@ class ContextAgent:
 
     def reset_listen_in_status(self):
         self.listen_in = False
+
+    def get_minutes_within_current_topic(self):
+        start_timestamp = self.minutes_agent.get_current_topic_start_timestamp()["timestamp"]
+        end_timestamp = self.minutes_agent.get_current_timestamp()
+        return self.minutes_agent.get_minutes_within_timestamp(start_timestamp, end_timestamp)
